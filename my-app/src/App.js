b23aca0b08f43f5880178c4c7aea6e9d60bd85e4
Riley Brunn <brunn.riley323@gmail.com>
2024-06-17 15:49:33 +0000

    first

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./components/examples/01_03";
 
 const App = () => {
-  return <div></div>;
+  return (
+  <div>
+    <Button label = 'click me'/>
+    </div>
+  );
 };
 
 export default App;
